332d71bf2ae483e29062b30f9b1a66b2464014ca
Matteo Piran <piranmatteo@gmail.com>
2024-03-31 11:01:55 +0200

    feat: update operators

--- operators.go
+++ operators.go
@@ -19,8 +19,20 @@ func ArrayToObject(exp interface{}) bson.M {
 	return bson.M{"$arrayToObject": exp}
 }
 
-func Push(exp interface{}) bson.M {
-	return bson.M{"$push": exp}
+func Push(exp ...interface{}) bson.M {
+	if len(exp) == 1 {
+		return bson.M{"$push": exp[0]}
+	}
+
+	if len(exp)%2 != 0 {
+		panic("Push expects an even number of arguments")
+	}
+
+	push := bson.M{}
+	for i := 0; i < len(exp); i += 2 {
+		push[exp[i].(string)] = exp[i+1]
+	}
+	return bson.M{"$push": push}
 }
 
 func First(exp interface{}) bson.M {
